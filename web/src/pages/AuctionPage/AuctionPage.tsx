6029bedf61f4fd57a738b0814b6d3c43ad935f79
David Thyresson <dthyresson@gmail.com>
2023-07-11 11:53:41 -0400

    Small UI tweaks

--- web/src/pages/AuctionPage/AuctionPage.tsx
+++ web/src/pages/AuctionPage/AuctionPage.tsx
@@ -27,44 +27,57 @@ const AuctionPage = ({ id = 1 }) => {
       <MetaTags title="Auction" description="Auction page" />
       <h1 className="text-xl font-bold">Auction Page</h1>
 
-      <AuctionCell id={auctionId} />
+      <div className="flex">
+        <div className="w-1/2 flex-initial">
+          <AuctionCell id={auctionId} />
+        </div>
+        <div className="w-1/2 flex-initial">
+          <form className="spacing-y-4 ">
+            <div>
+              Bin on:{' '}
+              <select
+                name="id"
+                defaultValue={auctionId}
+                onChange={(event) => setAuctionId(parseInt(event.target.value))}
+              >
+                <option value="1">Auction 1</option>
+                <option value="2">Auction 2</option>
+                <option value="3">Auction 3</option>
+                <option value="4">Auction 4</option>
+                <option value="5">Auction 5</option>
+              </select>
+            </div>
+            <div>
+              Bid Amount:{' '}
+              <input
+                name="bidAmount"
+                type="number"
+                defaultValue={amount}
+                onChange={(event) => {
+                  setAmount(parseInt(event.target.value))
+                }}
+              />
+            </div>
+            <div>
+              <button
+                className="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                onClick={(e) => {
+                  e.preventDefault()
 
-      <form>
-        <select
-          name="id"
-          defaultValue={auctionId}
-          onChange={(event) => setAuctionId(parseInt(event.target.value))}
-        >
-          <option value="1">Auction 1</option>
-          <option value="2">Auction 2</option>
-          <option value="3">Auction 3</option>
-          <option value="4">Auction 4</option>
-          <option value="5">Auction 5</option>
-        </select>
-        Bid Amount:{' '}
-        <input
-          name="bidAmount"
-          type="number"
-          defaultValue={amount}
-          onChange={(event) => {
-            setAmount(parseInt(event.target.value))
-          }}
-        />
-        <button
-          onClick={(e) => {
-            e.preventDefault()
-
-            bid({
-              variables: {
-                auctionId: auctionId,
-                amount: amount,
-              },
-            })
-          }}
-        >
-          Bid
-        </button>
-      </form>
+                  bid({
+                    variables: {
+                      auctionId: auctionId,
+                      amount: amount,
+                    },
+                  })
+                }}
+              >
+                Place Bid!
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
